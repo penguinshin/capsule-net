0d5317775bcad8e48cd7fa33d6c8eb4829f0e679
Ubuntu <ubuntu@ip-172-31-7-2.ec2.internal>
2017-11-29 05:47:06 +0000

    stuff

--- capsule.py
+++ capsule.py
@@ -103,34 +103,3 @@ class CapsNet(nn.Module):
 def debug_loss(input, target):
 
     return input.mean()
-
-def margin_loss(input, target, size_average=True):
-    
-    """
-    Class loss
-    Implement section 3 'Margin loss for digit existence' in the paper.
-    """
-    batch_size = input.size(0)
-    print(batch_size)
-    # Implement equation 4 in the paper.
-
-    # ||vc||
-    v_c = torch.sqrt((input**2).sum(dim=2, keepdim=True))
-
-    # Calculate left and right max() terms.
-    zero = Variable(torch.zeros(1)).detach()
-    m_plus = 0.9
-    m_minus = 0.1
-    loss_lambda = 0.5
-    max_left = torch.max(m_plus - v_c, zero).view(batch_size, -1)**2
-    max_right = torch.max(v_c - m_minus, zero).view(batch_size, -1)**2
-    t_c = target.type(torch.FloatTensor)
-    # Lc is margin loss for each digit of class c
-    
-    l_c = t_c * max_left + loss_lambda * (1.0 - t_c) * max_right
-    l_c = l_c.sum(dim=1)
-
-    if size_average:
-        l_c = l_c.mean()
-
-    return l_c
